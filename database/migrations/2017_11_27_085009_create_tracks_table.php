9b7a1a2be4ea89e804b167d426a0d495ee2cdce0
Vladimir Greshilov <greshilov@a2design.biz>
2017-11-27 15:11:10 +0600

    Added tracks table migration

---
+++ database/migrations/2017_11_27_085009_create_tracks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('filename');
+            $table->string('original_filename');
+            $table->string('title')->nullable();
+            $table->string('artist')->nullable();
+            $table->string('album')->nullable();
+            $table->string('year')->nullable();
+            $table->string('genre')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExist('tracks');
+    }
+}
