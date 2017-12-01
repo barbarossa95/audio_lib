5334bacc7426a231914afaeb480f999710ff0578
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-01 12:01:18 +0600

    Removed public app.js form git repo Added partial modal blade and track upload modal form

---
+++ resources/views/layouts/partial/modal.blade.php
@@ -0,0 +1,26 @@
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title"></h4>
+      </div>
+      <div class="modal-body">
+        <div class="preloader-container">
+          <div class="preloader-background"></div>
+            <div class="preloader">
+              <div style="width:100%;height:100%" class="lds-heart">
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div class="js-form-container"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
---
+++ resources/views/track/upload-modal-form.blade.php
@@ -0,0 +1,5 @@
+<div id="upload-form">
+    <form action="{{ action('TrackController@store') }}" onsubmit="javascript:void(0);">
+
+    </form>
+</div>
