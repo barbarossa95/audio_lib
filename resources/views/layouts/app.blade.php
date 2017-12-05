a86edb55d177b96cddbb423f230bbe6f809eac9b
v.greshilov <vgreshilov@mail.ru>
2017-12-05 21:28:28 +0600

    Added audio.js link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,6 +80,7 @@
     <script src={{ mix("/js/manifest.js") }}></script>
     <script src={{ mix("/js/vendor.js") }}></script>
     <script src="/js/laroute.js"></script>
+    <script src="/js/audio.min.js"></script>
     <script src={{ mix("/js/app.js") }}></script>
 
 </body>
