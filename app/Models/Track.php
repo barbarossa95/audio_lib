2dada57a558bf00c4f227b1e32ca8e549d631b3e
v.greshilov <vgreshilov@mail.ru>
2017-12-05 21:29:46 +0600

    Added model loggick

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -6,5 +6,73 @@ use Illuminate\Database\Eloquent\Model;
 
 class Track extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'original_filename',
+        'url',
+        'title',
+        'artist',
+        'album',
+        'year',
+        'genre'
+    ];
+
+    /**
+     * Save file from dropzone uploading to storage
+     *
+     * @param \Illuminate\Http\UploadedFile $file
+     * @return string $file
+     */
+    public static function saveFromDropzone($file)
+    {
+        $extension = $file->extension();
+        $directory = 'uploads/tracks';
+        $path = public_path($directory);
+        if (!is_dir($path)) mkdir($path, 0777, true);
+        $filename = self::generateUniqueName($file->getFilename());
+        $filename .= '.mp3';
+        try {
+            $file->move($path, $filename);
+            $track = self::create([
+                'original_filename' => $file->getClientOriginalName(),
+                'url' => url("$directory/$filename"),
+                'title' => null,
+                'artist' => null,
+                'album' => null,
+                'year' => null,
+                'genre' => null
+            ]);
+            return $track;
+        }
+        catch(Exeption $ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Generate unique name
+     *
+     * @param string $filename
+     * @return string
+     */
+    private static function generateUniqueName($filename)
+    {
+        return md5($filename . microtime());
+    }
+
+    /**
+     * Delee model instance with file from disc
+     *
+     * @return void
+     */
+    public function deleteWithFile()
+    {
+        unlink($this->url);
+        $this->delete();
+    }
 
 }
