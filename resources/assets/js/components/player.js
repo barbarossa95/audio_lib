63918fa23fe88d6f7c7410af7b865549432de765
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-08 16:03:02 +0600

    Minor changes

--- resources/assets/js/components/player.js
+++ resources/assets/js/components/player.js
@@ -1,24 +1,72 @@
-// Waiting audiojs library
-audiojs.events.ready(function() {
-    let $playlist = $('.playlist');
+import Vue from 'vue'
 
-    let as = audiojs.createAll({
-        trackEnded: function() {
-            let next = $playlist.find('li.playing').next();
-            if (!next.length) next = $playlist.find('li').first();
-            next.addClass('playing').siblings().removeClass('playing');
-            console.log(next);
-            player.load($('a', next).attr('data-src'));
-            player.play();
-        }
-    });
+Vue.component('player', {
+    mounted () {
+        // Waiting audiojs library
+        audiojs.events.ready(function() {
+            let $playlist = $('.playlist');
 
-    let player = as[0];
+            let as = audiojs.createAll({
+                trackEnded: function() {
+                    let next = $playlist.find('li.playing').next();
+                    if (!next.length) next = $playlist.find('li').first();
+                    next.addClass('playing').siblings().removeClass('playing');
+                    player.load($('a', next).attr('data-src'));
+                    player.play();
+                },
+                createPlayer: {
+                    markup: '\
+                  <div class="prev"></div>\
+                  <div class="play-pause"> \
+                    <p class="play"></p> \
+                    <p class="pause"></p> \
+                    <p class="loading"></p> \
+                    <p class="error"></p> \
+                  </div> \
+                  <div class="next"></div>\
+                  <div class="scrubber"> \
+                    <div class="progress"></div> \
+                    <div class="loaded"></div> \
+                  </div> \
+                  <div class="time"> \
+                    <em class="played">00:00</em>/<strong class="duration">00:00</strong> \
+                  </div> \
+                  <div class="error-message"></div>',
+                    playPauseClass: 'play-pause',
+                    scrubberClass: 'scrubber',
+                    progressClass: 'progress',
+                    loaderClass: 'loaded',
+                    timeClass: 'time',
+                    durationClass: 'duration',
+                    playedClass: 'played',
+                    errorMessageClass: 'error-message',
+                    playingClass: 'playing',
+                    loadingClass: 'loading',
+                    errorClass: 'error'
+                }
+            });
 
-    $playlist.find('li').click(function(e) {
-        e.preventDefault();
-        $(this).addClass('playing').siblings().removeClass('playing');
-        player.load($('a', this).attr('data-src'));
-        player.play();
-    });
+            let player = as[0];
+
+            $playlist.find('li').click(function(e) {
+                e.preventDefault();
+                $(this).addClass('playing').siblings().removeClass('playing');
+                player.load($('a', this).attr('data-src'));
+                player.play();
+            });
+
+            console.log('mounted');
+
+        });
+    }
 });
+
+
+
+
+/**
+ * init Vue at the element
+ */
+document.getElementById('js-vue-player') && new Vue({
+    el: '#js-vue-player'
+})
