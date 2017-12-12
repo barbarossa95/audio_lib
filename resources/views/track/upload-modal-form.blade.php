80382f41b758906f569886995c003d48908eb5bd
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-12 12:34:57 +0600

    Removed extra lines

--- resources/views/track/upload-modal-form.blade.php
+++ resources/views/track/upload-modal-form.blade.php
@@ -1,7 +1,4 @@
-<upload-form inline-template id="js-vue-upload" class='upgrades' v-cloak>
-    <form action="{{ action('TrackController@store') }}" onsubmit="javascript:void(0);">
-        <label for="test">Drop yours mp3 here:</label>
-        <div id="dropzone" class="dropzone dz-clickable">
-        </div>
-    </form>
-</upload-form>
+<form action="{{ action('TrackController@store') }}" onsubmit="javascript:void(0);">
+    <label for="test">Drop yours mp3 here:</label>
+    <div id="dropzone" class="dropzone dz-clickable"></div>
+</form>
