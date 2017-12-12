fb7ab2f88c539447591da29b9d5b9e742017bfbd
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-12 15:03:03 +0600

    Added loader and time

--- resources/views/layouts/partial/player.blade.php
+++ resources/views/layouts/partial/player.blade.php
@@ -1,11 +1,16 @@
 <player inline-template id="js-vue-player" class='player'>
     <div>
         <div v-cloak>
-            <audio muted></audio>
+            <div class="player_time">
+                <span ref="played"      class="player_time-played">00:00</span>
+                <span ref="duration"    class="player_time-duration">00:00</span>
+            </div>
+            <audio></audio>
             <div class="player_contols">
                 <div v-on:click="playPrev"      class="player_contols-prev" ><span class="glyphicon glyphicon-backward"></span></div>
                 <div v-on:click="togglePlay"    class="player_contols-pause" ><span class="glyphicon glyphicon-play"></span></div>
                 <div v-on:click="playNext"      class="player_contols-next" ><span class="glyphicon glyphicon-forward"></span></div>
+                <div v-on:click="toggleMuted"    class="player_contols-mute" ><span class="glyphicon glyphicon-volume-off"></span></div>
             </div>
             <ul ref="playlist" class="playlist" v-for="track in tracks">
                 <li>
@@ -13,6 +18,6 @@
                 </li>
             </ul>
         </div>
-        <div class="loader"></div>
+        <div ref="loader" class="loader"></div>
     </div>
 </player>
