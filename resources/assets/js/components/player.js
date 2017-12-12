3fd0274bdc2edf68bce3dd8c0135b6d287791ccd
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-12 11:26:11 +0600

    Refactored player component

--- resources/assets/js/components/player.js
+++ resources/assets/js/components/player.js
@@ -4,91 +4,38 @@ Vue.component('player', {
     data: function (){
         return {
             as: null,
-            player: null
+            player: null,
+            tracks: [],
         };
     },
 
-    beforeMount() {
-        console.log("beforeMount");
-    },
-
     mounted () {
+        console.log("mounted");
         let that = this; // vue instance
-        // Waiting audiojs library
+
         audiojs.events.ready(function() {
             let $playlist = $(that.$refs.playlist);
 
             let as = audiojs.createAll({
-                trackEnded: function() {
-                    let next = $playlist.find('li.playing').next();
-                    if (!next.length) next = $playlist.find('li').first();
-                    next.addClass('playing').siblings().removeClass('playing');
-                    player.load($('a', next).attr('data-src'));
-                    player.play();
-                },
-                createPlayer: {
-                    markup: '\
-                  <div class="play-controls"> \
-                      <div @click="playPrev" class="play-prev" ></div>\
-                      <div class="play-pause"> \
-                        <p class="play"></p> \
-                        <p class="pause"></p> \
-                        <p class="loading"></p> \
-                        <p class="error"></p> \
-                      </div> \
-                      <div @click="playNext" class="play-next"></div>\
-                  </div> \
-                  <div class="scrubber"> \
-                    <div class="progress"></div> \
-                    <div class="loaded"></div> \
-                  </div> \
-                  <div class="time"> \
-                    <em class="played">00:00</em>/<strong class="duration">00:00</strong> \
-                  </div> \
-                  <div class="error-message"></div>',
-                    playPauseClass: 'play-pause',
-                    scrubberClass: 'scrubber',
-                    progressClass: 'progress',
-                    loaderClass: 'loaded',
-                    timeClass: 'time',
-                    durationClass: 'duration',
-                    playedClass: 'played',
-                    errorMessageClass: 'error-message',
-                    playingClass: 'playing',
-                    loadingClass: 'loading',
-                    errorClass: 'error'
-                }
+                trackEnded: that.playNext,
             });
-
             that.player = as[0];
-
-            $playlist.find('li').click(function(e) {
-                e.preventDefault();
-                $(this).addClass('playing').siblings().removeClass('playing');
-                that.player.load($('a', this).attr('data-src'));
-                that.player.play();
-            });
         });
     },
 
     methods: {
-        playNext: function (event) {
-            console.log('playNext');
-            let $playlist = $(this.$refs.playlist);
-            this.player.pause();
-            let next = $playlist.find('li.playing').next();
-            if (!next.length) next = $playlist.find('li').first();
-            next.addClass('playing').siblings().removeClass('playing');
-            this.player.load($('a', next).attr('data-src'));
+        playTrack: function(trackSrc) {
+            this.player.load(trackSrc);
             this.player.play();
         },
 
-        playPrev: function (event) {
-            console.log('playPrev');
+        playNext: function (event) {
             this.player.pause();
         },
 
-
+        playPrev: function (event) {
+            this.player.pause();
+        },
     }
 });
 
