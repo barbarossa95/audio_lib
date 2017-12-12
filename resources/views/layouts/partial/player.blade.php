7ed7f0f8786617cf91279b6cf6eb3a021471ea06
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-12 17:14:19 +0600

    Added buttons

--- resources/views/layouts/partial/player.blade.php
+++ resources/views/layouts/partial/player.blade.php
@@ -6,12 +6,22 @@
                 <span ref="duration"    class="player_time-duration">00:00</span>
             </div>
             <audio></audio>
-            <div class="player_contols">
-                <div v-on:click="playPrev"      class="player_contols-prev" ><span class="glyphicon glyphicon-backward"></span></div>
-                <div v-on:click="togglePlay"    class="player_contols-pause" ><span class="glyphicon glyphicon-play"></span></div>
-                <div v-on:click="playNext"      class="player_contols-next" ><span class="glyphicon glyphicon-forward"></span></div>
-                <div v-on:click="toggleMuted"    class="player_contols-mute" ><span class="glyphicon glyphicon-volume-off"></span></div>
+            <div class="player_controls">
+                <div class="player_controls-left">
+                    <div v-on:click="toggleShuffle" class="player_controls-shuffle" ><span ref="shuffle" class="glyphicon glyphicon-random"></span></div>
+                    <div v-on:click="toggleRepeat" class="player_controls-repeat" ><span ref="repeat" class="glyphicon glyphicon-retweet"></span></div>
+                </div>
+                <div class="player_controls-center">
+                    <div v-on:click="playPrev"      class="player_controls-prev" ><span class="glyphicon glyphicon-step-backward"></span></div>
+                    <div v-on:click="togglePlay"    class="player_controls-pause" ><span ref="play" class="glyphicon glyphicon-play"></span></div>
+                    <div v-on:click="playNext"      class="player_controls-next" ><span class="glyphicon glyphicon-step-forward"></span></div>
+                </div>
+                <div class="player_controls-right">
+                    <div v-on:click="toggleMuted" class="player_controls-mute" ><span ref="mute" class="glyphicon glyphicon-volume-up"></span></div>
+                </div>
             </div>
+
+
             <ul ref="playlist" class="playlist" v-for="track in tracks">
                 <li>
                     <a href="#" :src="track.url" >@{{ track.original_filename }}</a>
