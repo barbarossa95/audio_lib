fe50accaef312b74107120d95324de9b6d17d85e
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-13 16:39:11 +0600

    Refactored markup

--- resources/views/layouts/partial/player.blade.php
+++ resources/views/layouts/partial/player.blade.php
@@ -7,20 +7,22 @@
             </div>
             <audio></audio>
             <div class="player_controls">
-                <div class="player_controls-left">
-                    <div v-on:click="toggleShuffle" class="player_controls-shuffle" ><span ref="shuffle" class="glyphicon glyphicon-random"></span></div>
-                    <div v-on:click="toggleRepeat" class="player_controls-repeat" ><span ref="repeat" class="glyphicon glyphicon-retweet"></span></div>
-                </div>
                 <div class="player_controls-center">
                     <div v-on:click="playPrev"      class="player_controls-prev" ><span class="glyphicon glyphicon-step-backward"></span></div>
                     <div v-on:click="togglePlay"    class="player_controls-pause" ><span ref="play" class="glyphicon glyphicon-play"></span></div>
                     <div v-on:click="playNext"      class="player_controls-next" ><span class="glyphicon glyphicon-step-forward"></span></div>
                 </div>
-                <div class="player_controls-right">
-                    <div v-on:click="toggleMuted" class="player_controls-mute" ><span ref="mute" class="glyphicon glyphicon-volume-up"></span></div>
+
+                <div class="player_controls-wrapper">
+                    <div class="player_controls-left">
+                        <div v-on:click="toggleShuffle" class="player_controls-shuffle" ><span ref="shuffle" class="glyphicon glyphicon-random"></span></div>
+                        <div v-on:click="toggleRepeat" class="player_controls-repeat" ><span ref="repeat" class="glyphicon glyphicon-retweet"></span></div>
+                    </div>
+                    <div class="player_controls-right">
+                        <div v-on:click="toggleMuted" class="player_controls-mute" ><span ref="mute" class="glyphicon glyphicon-volume-up"></span></div>
+                    </div>
                 </div>
             </div>
-
 
             <ul ref="playlist" class="playlist" v-for="track in tracks">
                 <li>
