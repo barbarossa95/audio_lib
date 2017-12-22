2b8ecdc45ffc2a2cc012200f0c99c6fe9554d9bb
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-22 08:46:43 +0600

    Fixed redirect route

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/track');
+            return redirect('/tracks');
         }
 
         return $next($request);
