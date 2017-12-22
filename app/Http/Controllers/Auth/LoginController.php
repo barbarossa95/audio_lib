6e6793eb02e2aa281b8b1ea4c37ec7b93cc1b056
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-22 08:55:20 +0600

    Updated redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/track';
+    protected $redirectTo = '/tracks';
 
     /**
      * Create a new controller instance.
