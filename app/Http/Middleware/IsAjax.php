ebadcd67ed185876f0bfad0b0461a399c8ef1666
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-22 08:47:44 +0600

    Added is ajax middleware

---
+++ app/Http/Middleware/IsAjax.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsAjax
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (!request()->ajax()) return redirect()->back();
+        return $next($request);
+    }
+}
