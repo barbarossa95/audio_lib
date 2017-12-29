9b9dc44177b5a9e815b51a18bae2267cf4f38fcf
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-29 10:39:18 +0600

    Added class bindings and is playing prop

--- resources/views/layouts/partial/playlist.blade.php
+++ resources/views/layouts/partial/playlist.blade.php
@@ -1,8 +1,18 @@
-<playlist inline-template id="js-vue-playlist" class='playlist' v-on:track-selected="trackSelected" ref="{{ $ref }}">
+<playlist inline-template id="js-vue-playlist" class='playlist' v-on:track-selected="trackSelected" v-bind:is-playing="isPlaying" ref="{{ $ref }}">
     <div>
         <div v-cloak>
-            <ul ref="playlist" v-for="track, index in tracks" :key="index">
-                <li class="playlist_item" v-on:click="selectTrack($event, index, track)">@{{ track.original_filename }}</li>
+            <ul ref="playlist">
+                <li class="playlist_item"
+                    v-for="track, index in tracks" :key="index"
+                    v-on:click="selectTrack($event, index, track)"
+                    v-bind:class="{ 'played': index === currentTrackIndex }" >
+                    <span class="playlist_item_playing glyphicon"
+                        v-bind:class="{
+                            'glyphicon-music': !(index === currentTrackIndex && isPlaying),
+                            'glyphicon-play': index === currentTrackIndex && isPlaying,
+                        }" >
+                    @{{ track.original_filename }}
+                </li>
             </ul>
         </div>
         <div ref="loader" class="loader"></div>
