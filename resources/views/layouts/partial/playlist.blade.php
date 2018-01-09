46708bd3bd5e52faaec8a62df9e5ccc6492d5bca
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-09 08:54:55 +0600

    Merge commit

--- resources/views/layouts/partial/playlist.blade.php
+++ resources/views/layouts/partial/playlist.blade.php
@@ -2,16 +2,21 @@
     <div>
         <div v-cloak>
             <ul ref="playlist">
-                <li class="playlist_item"
+                <li class="playlist_item clearfix"
                     v-for="track, index in tracks" :key="index"
                     v-on:click="selectTrack($event, index, track)"
                     v-bind:class="{ 'played': index === currentTrackIndex }" >
-                    <span class="playlist_item_playing glyphicon"
-                        v-bind:class="{
-                            'glyphicon-music': !(index === currentTrackIndex && isPlaying),
-                            'glyphicon-play': index === currentTrackIndex && isPlaying,
-                        }" >
-                    @{{ track.original_filename }}
+                    <div class="pull-left">
+                        <span class="playlist_item_playing glyphicon"
+                            v-bind:class="{
+                                'glyphicon-music': !(index === currentTrackIndex && isPlaying),
+                                'glyphicon-play': index === currentTrackIndex && isPlaying,
+                            }" >
+                        <span>
+                            @{{ track.original_filename }}
+                        </span>
+                    </div>
+                    <span class="pull-right glyphicon glyphicon-option-horizontal"></span>
                 </li>
             </ul>
         </div>
