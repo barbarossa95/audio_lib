5f77c73770cd89fc6c8b7dc20ff11af797e01b45
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-12 12:54:37 +0600

    Removed actions and store from file

--- resources/assets/js/store/mutations.js
+++ resources/assets/js/store/mutations.js
@@ -1,9 +1,3 @@
-export const state = {
-    tracks: [],
-    currentTrackIndex: -1,
-    currentTrack: null,
-};
-
 export const mutations = {
     setTracks (state, tracks) {
         state.tracks = tracks;
@@ -14,7 +8,6 @@ export const mutations = {
     },
 
     removeTrack (state, track) {
-
         state.tracks = _.without(state.tracks, track);
     },
 
@@ -31,34 +24,17 @@ export const mutations = {
 
     unshuffle (state) {
         state.tracks = _.sortBy(state.tracks, (o) => o.created_at);
+        state.currentTrackIndex = _.indexOf(state.tracks, state.currentTrack);
     },
 
     getNext (state) {
         if (++state.currentTrackIndex >= state.tracks.length) state.currentTrackIndex = 0;
         state.currentTrack = state.tracks[state.currentTrackIndex];
     },
-};
 
-export const actions = {
-    initStore (context, done) {
-        let url = laroute.route('track.index');
-
-        axios.get(url)
-        .then((response) => {
-            context.commit('setTracks', response.data);
-            done();
-        }).catch((error) => {
-            console.error(error);
-        });
+    getPrev (state) {
+        if (--state.currentTrackIndex < 0) state.currentTrackIndex = state.tracks.length-1;
+        state.currentTrack = state.tracks[state.currentTrackIndex];
     },
 
-    removeTrack (context, track) {
-        let url = laroute.route('track.destroy', {track : track.id});
-
-        axios.delete(url)
-            .then(response => {
-                if (track.id === state.currentTrack.id) context.commit('getNext');
-                context.commit('removeTrack', track)
-            });
-    },
 };
