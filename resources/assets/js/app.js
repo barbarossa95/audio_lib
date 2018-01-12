3af24304c65a0016eff9846ce88296eb66509e01
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-12 11:34:52 +0600

    Code format

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,16 @@
-require('./vendor/bootstrap');
+import Vue from 'vue'
+import bootstrap from './vendor/bootstrap'
+import Dropzone from 'dropzone'
 
-window.Dropzone = require('dropzone');
+//Components
+import $upload from './components/upload.js'
+import $playlist from './components/playlist.js'
+import $player from './components/player.js'
 
-$upload     = require('./components/upload.js');
-$playlist   = require('./components/playlist.js');
-$player     = require('./components/player.js');
+//Store
+import store from './store'
+
+document.getElementById('js-vue-player') && new Vue({
+    el: '#app',
+    store,
+})
