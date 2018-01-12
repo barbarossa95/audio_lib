2ed39b20019bad4874b2e8b8686cf39bd4dbf04f
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-12 13:34:15 +0600

    Refactored

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import bootstrap from './vendor/bootstrap'
-import Dropzone from 'dropzone'
+
+//Store
+import store from './store'
 
 //Components
 import $upload from './components/upload.js'
 import $playlist from './components/playlist.js'
 import $player from './components/player.js'
 
-//Store
-import store from './store'
 
 document.getElementById('js-vue-player') && new Vue({
     el: '#app',
