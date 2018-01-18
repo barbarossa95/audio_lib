eda40f21f634cd977d4113b0284f642786e452ab
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-18 16:31:08 +0600

    Added links to profile and tracks pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,12 +48,18 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ url('/tracks') }}">Tracks</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ url('/Profile') }}">
+                                            Profile
+                                        </a>
+                                    </li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
