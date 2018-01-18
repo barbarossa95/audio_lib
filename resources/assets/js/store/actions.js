83121dee40f4294a768b2043156ebb12d042da14
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-18 14:48:05 +0600

    Refactored actions

--- resources/assets/js/store/actions.js
+++ resources/assets/js/store/actions.js
@@ -1,23 +1,23 @@
 export const actions = {
-    initStore (context, done) {
+    initStore ({ commit }, done) {
         let url = laroute.route('track.index');
 
         axios.get(url)
         .then((response) => {
-            context.commit('setTracks', response.data);
+            commit('setTracks', response.data);
             done();
         }).catch((error) => {
             console.error(error);
         });
     },
 
-    removeTrack (context, track) {
+    removeTrack ({commit, getters}, track) {
         let url = laroute.route('track.destroy', {track : track.id});
 
         axios.delete(url)
             .then(response => {
-                if (context.currentTrack && track.id === context.currentTrack.id) context.commit('getNext');
-                context.commit('removeTrack', track)
+                if (getters.currentTrack && track.id === getters.currentTrack.id) commit('getNext');
+                commit('removeTrack', track);
             });
     },
 };
