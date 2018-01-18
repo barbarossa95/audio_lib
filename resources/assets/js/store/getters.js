7bd8f90dfadc9d407351d09baf279351a3e35b51
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-18 14:47:39 +0600

    Refactored getters, added getTrackById getter method

--- resources/assets/js/store/getters.js
+++ resources/assets/js/store/getters.js
@@ -1,5 +1,9 @@
 export const getters = {
-    currentTrack (state, getters) {
+    currentTrack: (state) => {
         return state.currentTrack;
+    },
+
+    getTrackById: (state) => (id) => {
+        return state.tracks.find(track => track.id === id)
     }
 };
