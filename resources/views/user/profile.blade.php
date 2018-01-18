8cc465fa37be6c3e0f2749500b04978ed7711840
Vladimir Greshilov <greshilov@a2design.biz>
2018-01-18 16:30:50 +0600

    Added template for user profile page

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading clearfix">
+                    <div class="pull-left">
+                        <h3>
+                            Your profile
+                        </h3>
+                    </div>
+                    <div class="pull-right">
+                        <div class="js-upload-track btn btn-success">
+                            Upload
+                            <div class="loader"></div>
+                        </div>
+
+                        @include('layouts.partial.modal', [
+                            'id' => 'uploadModal',
+                            'title' => 'Track upload' ])
+                    </div>
+                </div>
+
+                <div class="panel-body">
+profile content
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
